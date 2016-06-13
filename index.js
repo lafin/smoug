12f7e29d039d69be0d2f2079a23b5a5f211780f5
Kuvakin Sergey <kuvakin@gmail.com>
2016-06-14 00:20:55 +0300

    :walking:

--- index.js
+++ index.js
@@ -4,91 +4,133 @@ const readline = require("readline");
 const spawn = require("child_process").spawn
 const FileCookieStore = require("tough-cookie-filestore")
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-const url = "https://smo.com/"
-const smojar = request.jar(new FileCookieStore("smojar.json"))
-const vkjar = request.jar()
+const jarOne = request.jar(new FileCookieStore("jarOne.json"))
+const jarSecond = request.jar()
 
-const AuthURL = "https://oauth.vk.com"
-const APIURL = "https://api.vk.com"
-const APIVersion = "5.50"
+const firstUrl = "https://smofast.com/"
+const firstLogin = ""
+const firstPass = ""
+
+const secondAuthUrl = "https://oauth.vk.com"
+const secondApiUrl = "https://api.vk.com"
+const secondApiVersion = "5.50"
+const clientID = ""
+const secondLogin = ""
+const secondPass = ""
 
 
-
-
-const doAuthVk = (callback) => {
-  let options = {
-    url: AuthURL + "/authorize?client_id=" + clientID + "&redirect_uri=https://oauth.vk.com/blank.html&display=mobile&scope=wall,groups&v=&response_type=token&v=" + APIVersion,
-    jar: vkjar
-  }
-  return request(options, (error, response, body) => {
-    let urlStr = body.match(/<form method=\"post\" action=\"(.*?)\">/mi)
-    urlStr = urlStr[1]
-    console.log(urlStr);
-
-    const re = /<input type=\"hidden\" name=\"(.*?)\" value=\"(.*?)\"\s?\/?>/gm
-    let value
-    let formData = {}
-    while ((value = re.exec(body)) !== null) {
-      formData[value[1]] = value[2]
-    }
-    formData["email"] = email
-    formData["pass"] = pass
-
-    options = {
-      url: urlStr,
-      method: "post",
-      form: formData,
-      jar: vkjar
-    }
-    return request(options, (error, response, body) => {
-      let href = response.request.href
-      let token = href.match(/access_token=(.*?)&/mi)
-      return callback(token[1])
-    })
-  })
+function pr(options) {
+  return new Promise(
+    function(resolve, reject) {
+      request(options,
+        (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve({
+              response: response,
+              body: body
+            });
+          }
+        });
+    });
 }
 
-const doAuth = (captchaUrl) => {
-  request({
-    url: captchaUrl,
-    jar: smojar
-  }).pipe(fs.createWriteStream("/tmp/captcha.png"))
-  setTimeout(() => spawn("open", ["/tmp/captcha.png"]), 2e3)
+function doAuthSecond() {
+  return pr({
+      url: secondAuthUrl + "/authorize?client_id=" + clientID + "&redirect_uri=" + secondAuthUrl + "/blank.html&display=mobile&scope=wall,groups&v=&response_type=token&v=" + secondApiVersion,
+      jar: jarSecond
+    })
+    .then((data) => {
+      let urlStr = data.body.match(/<form method=\"post\" action=\"(.*?)\">/mi)
+      urlStr = urlStr[1]
+      console.log(urlStr);
 
-  return rl.question("captcha? ", (answer) => {
-    let captcha = answer
-    rl.close();
-
-    options = {
-      url: url,
-      method: "post",
-      form: {
-        login: smoLogin,
-        pass: smoPass,
-        captcha: captcha,
-        remember: "on",
-        connect: "Вход"
-      },
-      jar: smojar
-    }
-
-    return request(options, (error, response, body) => {
-      error = /Ошибка/.test(body)
-      if (!error) {
-        return getTasks()
+      const re = /<input type=\"hidden\" name=\"(.*?)\" value=\"(.*?)\"\s?\/?>/gm
+      let value
+      let formData = {}
+      while ((value = re.exec(data.body)) !== null) {
+        formData[value[1]] = value[2]
       }
+      formData["email"] = secondLogin
+      formData["pass"] = secondPass
+
+      return pr({
+          url: urlStr,
+          method: "post",
+          form: formData,
+          jar: jarSecond
+        })
+        .then((data) => {
+          let href = data.response.request.href
+          let token = href.match(/access_token=(.*?)&/mi)
+          if (token && token.length) {
+            return token[1]
+          } else {
+            throw new Error("getting a token")
+          }
+        })
+        .catch(error => {
+          console.log("error", error)
+        })
     })
-  })
+    .catch(error => {
+      console.log("error", error)
+    })
 }
 
-const getGroupInfo = (groupName, callback) => {
+function doAuthOne(captchaUrl) {
+  return pr({
+      url: captchaUrl,
+      jar: jarOne
+    })
+    .then(fs.createWriteStream("/tmp/captcha.png"))
+    .then(() => {
+      setTimeout(() => spawn("open", ["/tmp/captcha.png"]), 2e3)
+    })
+    .then(() => {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      })
+      return rl.question("captcha? ", (answer) => {
+        let captcha = answer
+        rl.close()
+
+        return pr({
+            url: firstUrl,
+            method: "post",
+            form: {
+              login: firstLogin,
+              pass: firstPass,
+              captcha: captcha,
+              remember: "on",
+              connect: "Вход"
+            },
+            jar: jarOne
+          })
+          .then((data) => {
+            error = /Ошибка/.test(data.body)
+            if (error) {
+              throw new Error()
+            } else {
+              return doStepSecond()
+            }
+          })
+          .catch(error => {
+            console.log("error", error)
+          })
+      })
+    })
+    .catch(error => {
+      console.log("error", error)
+    })
+}
+
+function getGroupInfo(groupName, callback) {
   let options = {
-    url: APIURL + "/method/groups.getById?&group_id=" + groupName + "&fields=&v=" + APIVersion,
-    jar: vkjar,
+    url: secondApiUrl + "/method/groups.getById?&group_id=" + groupName + "&fields=&v=" + secondApiVersion,
+    jar: jarSecond,
     json: true
   }
   return request(options, (error, response, body) => {
@@ -96,10 +138,10 @@ const getGroupInfo = (groupName, callback) => {
   })
 }
 
-const doJoinVk = (groupID, token, callback) => {
+function doJoinVk(groupID, token, callback) {
   let options = {
-    url: APIURL + "/method/groups.join?&group_id=" + groupID + "&access_token=" + token + "&v=" + APIVersion,
-    jar: vkjar,
+    url: secondApiUrl + "/method/groups.join?&group_id=" + groupID + "&access_token=" + token + "&v=" + secondApiVersion,
+    jar: jarSecond,
     json: true
   }
   console.log("join:", groupID)
@@ -108,10 +150,10 @@ const doJoinVk = (groupID, token, callback) => {
   })
 }
 
-const doLeaveVk = (groupID, token, callback) => {
+function doLeaveVk(groupID, token, callback) {
   let options = {
-    url: APIURL + "/method/groups.leave?&group_id=" + groupID + "&access_token=" + token + "&v=" + APIVersion,
-    jar: vkjar,
+    url: secondApiUrl + "/method/groups.leave?&group_id=" + groupID + "&access_token=" + token + "&v=" + secondApiVersion,
+    jar: jarSecond,
     json: true
   }
   console.log("leave:", groupID)
@@ -120,16 +162,16 @@ const doLeaveVk = (groupID, token, callback) => {
   })
 }
 
-const doRepost = (idGroup, nameGroup, token) => {
+function doRepost(idGroup, nameGroup, token) {
   options = {
-    url: url + "system/modules/vk/process.php",
+    url: firstUrl + "system/modules/vk/process.php",
     method: "post",
     form: {
       get: 1,
       url: nameGroup,
       pid: idGroup
     },
-    jar: smojar
+    jar: jarOne
   }
 
   return request(options, (error, response, body) => {
@@ -161,37 +203,47 @@ const doRepost = (idGroup, nameGroup, token) => {
   })
 }
 
-const getTasks = () => {
-  doAuthVk(token => {
-    if (token) {
-      options = {
-        url: url + "p.php?p=vk",
-        jar: smojar
+function doStepSecond(token) {
+  return pr({
+      url: firstUrl + "p.php?p=vk",
+      jar: jarOne
+    })
+    .then((data) => {
+      const re = /ModulePopup\('(.*?)','(.*?)',.*\);/gm
+      let values = []
+      let value
+      while ((value = re.exec(data.body)) !== null) {
+        values = values.concat([value[1], value[2], token]);
       }
-      return request(options, (error, response, body) => {
-        const re = /ModulePopup\('(.*?)','(.*?)',.*\);/gm
-        let value
-        while ((value = re.exec(body)) !== null) {
-          doRepost(value[1], value[2], token)
-        }
-      })
-    } else {
-      console.log("Doesn't token")
-    }
-  })
+      return values
+    })
+    .catch(error => {
+      console.log("error", error)
+    })
+
 }
 
-let options = {
-  url: url,
-  jar: smojar
-}
-return request(options, (error, response, body) => {
-  let captchaUrl = body.match(/<img\s+src=\"(.*?)\"\s+alt=\"\"\/\>/mi)
-  captchaUrl = captchaUrl && (url + captchaUrl[1])
-  if (captchaUrl) {
-    console.log("captcha url:", captchaUrl)
-    return doAuth(captchaUrl)
-  } else {
-    return getTasks()
-  }
-})
+pr({
+    url: firstUrl,
+    jar: jarOne
+  })
+  .then(data => {
+    let captchaUrl = data.body.match(/<img\s+src=\"(.*?)\"\s+alt=\"\"\/\>/mi)
+    captchaUrl = captchaUrl && (firstUrl + captchaUrl[1])
+    if (captchaUrl) {
+      console.log("captcha url:", captchaUrl)
+      return doAuthOne(captchaUrl)
+    }
+  })
+  .then(data => {
+    return doAuthSecond()
+  })
+  .then(token => {
+    return doStepSecond(token)
+  })
+  .then(values => {
+    console.log(values)
+  })
+  .catch(error => {
+    console.log("error", error)
+  })
